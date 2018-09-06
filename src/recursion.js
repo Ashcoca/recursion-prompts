7aa89d5a943bacbb6743b298abd9cd42eb9e2287
Ashvin Coca <AshvinCoca@gmail.com>
2018-09-06 13:00:23 -0700

    Cleaned up, all donzo!

--- src/recursion.js
+++ src/recursion.js
@@ -215,10 +215,14 @@ var replaceKeysInObj = function(obj, oldKey, newKey) {
 // fibonacci(5); // [0,1,1,2,3,5]
 // Note: The 0 is not counted.
 var fibonacci = function(n) {
-  if (n <= 0) return null;
-  if (n == 1) return [0, 1];
-  const arr = fibonacci(n - 1);
-  return [...arr, arr[n-1] + arr[n-2]];
+  if (n <= 0) {
+     return null
+  };
+  if (n == 1) {
+     return [0, 1]
+  };
+  var arr = fibonacci(n - 1)
+  return [...arr, arr[n-1] + arr[n-2]]
 };
 
 // 26. Return the Fibonacci number located at index n of the Fibonacci sequence.
